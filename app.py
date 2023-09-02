7e335a3eade1b2792a3235f98a5a3d809d2b02e0
Mark Anthony Pequeras <pequeras@icloud.com>
2023-09-01 20:17:03 -0700

    updates

--- app.py
+++ app.py
@@ -8,16 +8,22 @@ from flask import Flask, render_template, request, jsonify, session, redirect, u
 import flask
 
 version = "1.4"
-
 app = Flask(__name__,
             static_url_path='',
             static_folder='static',
             template_folder='templates')
-
-
 app.secret_key = '\xb2\xcb\x06\x85\xb1\xcfZ\x9a\xcf\xb3h\x13\xf6\xa6\xda)\x7f\xdd\xdb\xb2BK>'
 
 # Owner Dashboard
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    """
+    main page
+    """
+    return render_template("dashboard.html")
 
 
 @app.route("/dashboard", methods=['POST', 'GET'])
@@ -27,55 +33,49 @@ def dashboard():
     if 'authenticated' in session:
         if len(session['authenticated']):
             return render_template("dashboard.html", data=dt)
-
     return redirect(url_for("login"))
 
 
 # Start - Route functions
 
 @app.route("/inquire", methods=['POST', 'GET'])
-def messageRec():
+def messagereceive():
     try:
         data = request.json
         json = data
-
         name = json['name']
         email = json['email']
         subject = json['subject']
         message = json['message']
-
         con = sqlite3.connect("knightstudiomsg")
         cur = con.cursor()
         params = "INSERT INTO Messages (Name,Subject,Email,Message) VALUES (?,?,?,?)"
         vals = (name, subject, email, message)
-
         cur.execute(params, vals)
-
         con.commit()
         con.close()
-
         return jsonify({"result": 1})
-
     except Exception as e:
         print(e)
         return jsonify({"result": 0})
 
 
-@app.route("/logout")
+@app.route("/logoff")
 def logout():
-    del session['authenticated']
-    return "1"
+    try:
+        del session['authenticated']
+    except Exception as e:
+        pass
+    return redirect(url_for("index"))
 
 
 @app.route("/messages")
 def message():
-
     con = sqlite3.connect("knightstudiomsg")
     cur = con.cursor()
     ret = cur.execute("SELECT * FROM MESSAGES")
     data = ret.fetchall()
     con.close()
-
     return render_template("messages.html", data=data)
 
 
@@ -88,12 +88,10 @@ def login():
     if request.method == 'POST':
         _u = request.form.get('uname')
         _p = request.form.get('pwd')
-
         try:
             _de = dataengine.knightclient()
             _cred = _de.get_cred(_u, _p)
             _cred_data = _cred[0]
-
             if _cred_data[0] == _u and _cred_data[1] == _p:
                 print("Auth success")
                 # Set session
@@ -102,15 +100,12 @@ def login():
             else:
                 print("Auth Failed")
                 return render_template("login.html", error=True)
-
         except Exception as e:
             print("Error: ", e)
             return render_template("login.html", error=True)
-
     if 'authenticated' in session:
         if len(session['authenticated']):
             return redirect(url_for("dashboard"))
-
     return render_template("login.html", error=False)
 
 
