ff107abd42ea383740e8ab85ce5b953da9020e42
Mark Anthony Pequeras <pequeras@icloud.com>
2023-10-10 21:34:18 -0700

    updates product page

--- static/SYSTEM/default/js/product.js
+++ static/SYSTEM/default/js/product.js
@@ -7,10 +7,14 @@ const varselect = document.querySelector('select');
             var seloption = varbox.options[varbox.selectedIndex].text;
 
             let stockp = document.getElementById('stock');
+            let pricep = document.getElementById('pricep');
+
             if (seloption === "Available variants"){
                   stockp.innerText = "";
+                  pricep.innerText = `Price: \$${mainprice}`;
             }else{
             console.log(seloption)
                   stockp.innerText = `Variant: ${seloption} - Available in stock: ${productinfo[seloption+"-ivar"]['instock']}`;
+                  pricep.innerText = `Price: \$${productinfo[seloption+"-ivar"]['price']}`;
             }
 });
