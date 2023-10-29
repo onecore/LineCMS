69b92c01b193eb29934a61f3b27244f4f1fe29bd
Mark Anthony Pequeras <pequeras@icloud.com>
2023-10-28 21:09:20 -0700

    updates moved other dashboard funcs

--- engine/product.py
+++ engine/product.py
@@ -7,10 +7,19 @@ import os
 from icecream import ic
 from helpers import currency
 from helpers import country
+from decimal import Decimal
+import stripe
+
+product = Blueprint("product", __name__)
+
+
+ps = dataengine.knightclient()
+global shipcountries, shiprates, shipstatus
+sk, pk, ck, _, wk, wsk,shipstatus,shiprates,shipcountries = ps.productsettings_get() # wk is not needed
+stripe.api_key = sk
 
 UPLOAD_FOLDER_PRODUCTS = 'static/dashboard/uploads/products'
 
-product = Blueprint("product", __name__)
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'svg', 'ico'])
 
@@ -209,3 +218,171 @@ def product_orders_single(id):
     return render_template("/dashboard/product-orders-single.html", order=order,alert=alert,items=parseditems,shipping_fee=shipping_fee)
 
 
+
+# Product webhook and other dashboard funcs
+
+def price(price) -> int:
+    "Stripe friendly price"
+    o = round(Decimal(price)*100) # Decimal to keep 2 decimal places from html input, Float doesn't work as it doesn keep the decimals
+    return o
+
+
+def parserate(name,amount,mins,maxs):
+    clone =  {
+                "shipping_rate_data": {
+                        "type": "fixed_amount",
+                        "fixed_amount": {"amount": price(amount), "currency": ck},
+                        "display_name": name,
+                        "delivery_estimate": {
+                            "minimum": {"unit": "business_day", "value": int(mins)},
+                            "maximum": {"unit": "business_day", "value": int(maxs)},
+                   },
+                },
+            }
+    return clone
+
+def ratetemplater(obj):
+    options = []
+    parsedobj = None
+    try:
+        parsedobj = eval(obj)
+    except:
+        print("Unable to parse ship rates")
+        parsedobj = {}
+    if parsedobj:
+        for rate_name,rate_data in parsedobj.items():
+            options.append(parserate(rate_name,rate_data[0],rate_data[1],rate_data[2]))
+        return options
+    return []
+
+def deductquant(id,variant,quantity):
+    pass
+    
+@product.route('/event', methods=['POST'])
+def new_event():
+    """
+    Stripe webhook
+    """
+    sk, pk, ck, _, wk, wsk,shipstatus,shiprates,shipcountries = ps.productsettings_get() # wk is not needed
+
+    event = None
+    payload = request.data
+    signature = request.headers['STRIPE_SIGNATURE']
+    try:
+        event = stripe.Webhook.construct_event(payload, signature, wsk)
+    except Exception as e:
+        ic(e)
+    if event['type'] == 'checkout.session.completed':
+        session = stripe.checkout.Session.retrieve(event['data']['object'].id, expand=['line_items'])
+        items = []
+        for item in session.line_items.data:
+            items.append([item.description,item.quantity])
+        
+        order = {
+                "customer_name":session.customer_details.name,
+                "customer_email":session.customer_details.email,
+                "amount_total":  f'${session.amount_total/100:.02f} {item.currency.upper()}',
+                "created":session.created,
+                "payment_status": session.payment_status,
+                "customer_country": session.customer_details.address.country,
+                "customer_postal": session.customer_details.address.postal_code,
+                "currency": session.currency,
+                "items": str(items),
+                "session_id": session.id,
+                
+                "metadata": session.metadata,
+                "address": session.customer_details.address.line1,
+                "phone": session.customer_details.phone,
+                "shipping_cost": ""
+                }
+        if shipstatus == "on":
+            order["shipping_cost"] = session.shipping_cost.amount_total
+        de = dataengine.knightclient()        
+        de.productorders_set(order)
+    return {'success': True}
+
+
+def check(data):
+    _, _, _, _, _, _,shipstatus,shiprates,shipcountries = ps.productsettings_get() # wk is not needed
+    
+    _de = dataengine.knightclient()
+    items = []
+    load_items = []
+    product_meta = {}
+    for product, values in data.items():
+        _, _price, _quantity, _variant = values.split(",")
+        def includevariant():
+            selected_variant = ""
+            if _variant != "Available variants":
+                selected_variant = _variant
+                return f" - Variant: {selected_variant}"
+            return selected_variant
+            
+        product_data = _de.get_product_single(route=False, checkout=product)
+        product_meta[product_data[1] + includevariant()] = product
+        clone = {
+                'price_data': {
+                    'product_data': {
+                        'name': product_data[1] + includevariant(),
+                    },
+                    'unit_amount': price(_price),
+                    'currency': ck.lower(),
+                },
+                'quantity': int(_quantity),
+                }
+
+        items.append(clone)
+
+    if shipstatus == "on":  # If shipping Enabled
+        
+        def parsetolist(shipcountries):
+            if "," in shipcountries:
+                return shipcountries.split(",")
+            if len(shipcountries):
+                return [shipcountries]
+            return ["US","CA"]
+        
+        shipratesparsed = ratetemplater(shiprates)    
+        shipcountries = parsetolist(shipcountries)
+        checkout_session = stripe.checkout.Session.create(
+            # Below parameters enable shipping
+            shipping_address_collection={"allowed_countries": shipcountries},
+            shipping_options=shipratesparsed,
+            line_items=items,
+            payment_method_types=['card'],
+            mode='payment',
+            success_url=request.host_url + 'order/success',
+            cancel_url=request.host_url + 'order/cancel',
+            metadata = product_meta,
+        )
+    
+    else:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=items,
+            payment_method_types=['card'],
+            mode='payment',
+            success_url=request.host_url + 'order/success',
+            cancel_url=request.host_url + 'order/cancel',
+            metadata=product_meta
+        )
+    
+    ic(product_meta)
+    return checkout_session.url
+
+
+@product.route('/product-checkout', methods=['GET', 'POST'])
+def prodcheck():
+    if request.method == "POST":
+        prdata = {}
+        try:
+            prdata = eval(request.data)
+        except Exception as e:
+            return jsonify({"status": 0})
+
+        if prdata:
+            clientres = check(prdata)
+            return jsonify({'c': clientres})
+
+        return jsonify({"status": 0})
+    else:
+        return jsonify({"status": 0})
