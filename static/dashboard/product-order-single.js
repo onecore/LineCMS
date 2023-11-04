73cc698e050fe5bd4395e672b9841eba45b0c48a
Mark Anthony Pequeras <pequeras@icloud.com>
2023-11-04 15:22:48 -0700

    update

--- static/dashboard/product-order-single.js
+++ static/dashboard/product-order-single.js
@@ -1,14 +1,15 @@
-var custom_template = false;
+var custom_template_status = false;
+var custom_template = null;
 
 function ckdata_get(){
     let bel = document.getElementById("customtemp_stat")
     if (bel.textContent === "Save for this order"){
         let ck = CKEDITOR.instances.ckeditor.getData();
-        custom_template = ck;
+        custom_template_status = ck;
         bel.textContent = "Use default"
     }
     else{
-        custom_template = false;
+        custom_template_status = false;
         bel.textContent = "Save for this order"
     }
 }
