06f73092265d4961a4a1ca963f9c81616f6f8b1c
Mark Anthony Pequeras <pequeras@icloud.com>
2023-11-05 09:39:58 -0800

    update

--- helpers/emailparser.py
+++ helpers/emailparser.py
@@ -8,22 +8,18 @@ from flask_mail import Message, Mail
 from flask import jsonify
 from flask import current_app
 
-# from app import sendmail
 d = dataengine.knightclient()
 logger = d.log
 
-sk, pk, ck, _, wk, wsk,shipstatus,shiprates,shipcountries,_,_,_,_,sender,_ = d.productsettings_get() # wk is not needed
-
-
+sk, pk, ck, _, wk, wsk,shipstatus,shiprates,shipcountries,_,_,_,_,sender,_ = d.productsettings_get()
 
 def price(price) -> int:
-    "Stripe friendly price"
-    o = round(Decimal(price)*100) # Decimal to keep 2 decimal places from html input, Float doesn't work as it doesn keep the decimals
+    "parces stripe int into decimal (unlike float this keeps the 2 decimal places)"
+    o = round(Decimal(price)*100) 
     return o
 
-def data(which,order,company,shipstatus,tracking=False):
-    
-    "tracking parameter only for fulfilled template"
+def data(which,order,company,shipstatus,tracking=False) -> dict:
+    """adds all the jinja templating values"""
     formatter = {
                 "COMPANYNAME":company['sitename'],"COMPANYNUMBER":company['sitenumber'],"COMPANYEMAIL":company['siteemail'],
                 "ORDERNUMBER":order['ordernumber'],"ORDERTOTAL":order['amount_total'],"ORDERDATE":order['created'],"CUSTOMERADDRESS":order['address'],"CUSTOMERNAME":order['customer_name']
@@ -32,8 +28,10 @@ def data(which,order,company,shipstatus,tracking=False):
         formatter['TRACKINGLINK'] = tracking
     return formatter
 
-def parse_send(**kwargs) -> bool:    
-    with current_app.app_context():
+def parse_send(**kwargs) -> bool:
+    "parses data into email friendly format"   
+     
+    with current_app.app_context(): # Cannot import from flask app obj without this
         from app import sendmail        
         
     if kwargs:
@@ -42,16 +40,16 @@ def parse_send(**kwargs) -> bool:
             subobj = {"fulfilled": f"Hi {kwargs['order']['customer_name']} Your order is on the way! ", "placed": f"Hi {kwargs['order']['customer_name']} Your order is placed! "}
             
             t_settings = le(kwargs['ps'][12])
-            if "template" in kwargs: # Change template template
+            if "template" in kwargs: # Change template to Custom template
                 temps[kwargs['which']] = kwargs['template']
                 
+            
             template = Template(temps[kwargs['which']])
-            rendered = template.render(data(kwargs['which'],kwargs['order'],kwargs['company'],False))
+            rendered = template.render(data(kwargs['which'],kwargs['order'],kwargs['company'],False)) # tracking is False (perfect for fulfilled call)
             
             subject = subobj[kwargs['which']]
             recip = kwargs['order']['customer_email']
             sendr = le(kwargs['ps'][13])['email']
-            
             sendmail(subject=subject,reciever=recip,html=rendered,sender=sendr)
                 
         except Exception as e:
