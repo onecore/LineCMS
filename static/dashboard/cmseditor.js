a1337009fd99b59c0df09035fdd7630f892c2f73
Mark Anthony Pequeras <pequeras@icloud.com>
2023-12-03 14:38:11 -0800

    sel2 updates

--- static/dashboard/cmseditor.js
+++ static/dashboard/cmseditor.js
@@ -5,6 +5,7 @@ const sel2 = document.getElementById("sel2")
 const sel3 = document.getElementById("sel3")
 
 
+
 function sourceupdate(file,pattern,load=false){
     var d = {};
     if (load){
@@ -25,6 +26,10 @@ function sourceupdate(file,pattern,load=false){
 
 }
 
+function loader_init(){
+
+}
+
 sel1.addEventListener('change', function (e) {
     let val_ = e.target.value;
     if (val_ === "th"){
@@ -35,6 +40,7 @@ sel1.addEventListener('change', function (e) {
             option.value = "th";
             sel2.appendChild(option)
         }
+    sel2.dispatchEvent(new Event('change'));
     }else if (val_ === "ts"){
         sel2.innerHTML = "";
         for (let o in temps) {
@@ -50,22 +56,30 @@ sel1.addEventListener('change', function (e) {
             option.text = "Sitemap/Robots";
             option.value = "sr";
             sel2.appendChild(option)
-
+    sel2.dispatchEvent(new Event('change'));
     }else if (val_ === "py"){
+        sel3.innerHTML = "";        
         sel2.innerHTML = "";
+        var option2 = document.createElement("option");
+        option2.text = "enginepublic"
+        option2.value = "enginepublic"
+        sel2.appendChild(option2)
+
         for (let o in pyfiles) {
             var option = document.createElement("option");
             option.text = pyfiles[o];
             option.value = "th";
-            sel2.appendChild(option)
+            sel3.appendChild(option)
         }
-
+    sel2.dispatchEvent(new Event('change'));
     }else if (val_ === "sf"){
         sel2.innerHTML = "";
         let option = document.createElement("option");
             option.text = "settings";
             option.value = "sf";
             sel2.appendChild(option)
+    sel2.dispatchEvent(new Event('change'));
+
     }
     
     
@@ -73,7 +87,33 @@ sel1.addEventListener('change', function (e) {
 
 sel2.addEventListener('change', function (e) {
     let val_ = e.target.value;
-    alert("asd")
+    sel1_val = sel1.value;
+
+    switch (sel1_val) {
+        case "sf":
+            sel3.innerHTML = "";
+            let option = document.createElement("option");
+            option.text = "settings.py";
+            option.value = "sf";
+            sel3.appendChild(option)
+            break;
+
+        case "sr":
+            sel3.innerHTML = "";
+            let k = ['Sitemap.xml','Robots.txt']
+            for (let o in k){
+                let option = document.createElement("option");
+                option.text = k[o];
+                option.value = k[o];
+                sel3.appendChild(option)
+            }
+
+            break;
+        
+    
+        // default:
+        //     break;
+    }
 });
 
 sel3.addEventListener('change', function (e) {
