e08ee5408d9e6b7e18d75faa2261b82f031998c1
Mark Anthony Pequeras <pequeras@icloud.com>
2023-12-05 20:35:08 -0800

    added installed col in db

---
+++ enginepublic/install.py
@@ -0,0 +1,8 @@
+"""
+SandCMS - Content Management System (Product & Blogging) for Rapid website development
+Website: www.sandcms.com
+Author: S. Jangra & Mark A.R. Pequeras
+"""
+
+from settings import cms_version
+import dataengine as LineCMS
